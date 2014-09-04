9034f433de50657932ec758b2a445f17c43c7851
jakelamotta <kristian.johansson86@gmail.com>
2014-09-04 17:06:27 +0200

    Added a few more classes
    Also implemeneted the XLSWriter which should be done, might remove the
    abstract super class as the xlswriter is very simple

--- dataconverter/main.m
+++ dataconverter/main.m
@@ -1,7 +1,18 @@
-function [ output_args ] = main()
-%MAIN Summary of this function goes here
-%   Detailed explanation goes here
+function main()
+%MAIN this is the starting point of the dataconverter program
 
+handler = GUIHandler;
+mnger = DataManager;
 
+handler.manager = mnger;
+handler.launchGUI('mainwindow');
+
+while true
+
+    if handler.exit
+        break;
+    end
+    pause(0.1);
 end
 
+end
--- dataconverter/model/XLSWriter.m
+++ dataconverter/model/XLSWriter.m
@@ -3,11 +3,16 @@ classdef XLSWriter < AbstractWriter
     %(excel)
     
     properties
+        dataObject
     end
     
     methods (Access = private)
-        function success = writeToXLS(dataObj)
+        
+        function success = writeToXLS(this)
+            xlswrite(fileName,this.dataObject);
+            success = exist(fileName,'file');
         end
+        
     end
     
 end
