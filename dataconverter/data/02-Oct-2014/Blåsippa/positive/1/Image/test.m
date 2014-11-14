69bb3be955586003b8c2b953c51c6401d406958b
josda <josefin.dahlbom@neuro.uu.se>
2014-11-14 14:25:19 +0100

    nåt

--- dataconverter/data/02-Oct-2014/Blåsippa/positive/1/Image/test.m
+++
@@ -1,8 +0,0 @@
-k = getappdata(0,'v');
-
-while k < 10
-    k = k+1;
-    setappdata(0,'v',k);
-    drawnow;
-    pause(1);
-end
--- dataconverter/data/14-Oct-2014/tussilago/positive/tus_p_14-Oct-2014_7923/Spectro/uitest.m
+++
@@ -1,9 +0,0 @@
-function uitest(varargin)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    t = varargin{3};
-    
-    disp('called back');
-    disp(get(t,'selected'));
-end
-
